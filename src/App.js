e8770100f983bed83ab2d56268bf66c035d828fd
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-21 16:17:40 +0600

    circle and buttons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className='flex justify-center items-center h-screen'>
+        <div className={`h-[50px] w-[50px] rounded-full bg-green-700`}>
+        </div>
+      </div>
+
+      <div>
+        <button className='bg-green-700 text-white mr-2'>top</button>
+        <button  className='bg-green-700 text-white mr-2'>bottom</button>
+        <button className='bg-green-700 text-white mr-2'>left</button>
+        <button className='bg-green-700 text-white mr-2'>right</button>
+      </div>
     </div>
   );
 }
